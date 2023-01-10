7714e94cb1a7c7a3ddbd452d622d94cec3981838
hululu1068 <xjianglin@gmail.com>
2023-01-10 22:00:04 +0800

    update ax6

--- diy-part2-ax6.sh
+++ diy-part2-ax6.sh
@@ -21,23 +21,6 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 # 修改时区
 sed -i "s/UTC/CST-8/g" package/base-files/files/bin/config_generate
 
-# themes添加（svn co 命令意思：指定版本如https://github）
-# git clone https://github.com/jerrykuku/luci-theme-argon.git package/custom-package/luci-theme-argon
-# git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon.git package/custom-package/luci-theme-atmaterial-ColorIcon
-# git clone https://github.com/thinktip/luci-theme-neobird.git package/custom-package/luci-theme-neobird
-
-# 添加额外软件包
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/custom-package/luci-app-adguardhome
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/custom-package/openwrt-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/custom-package/luci-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/custom-package/luci-passwall2
-git clone --depth=1 https://github.com/fw876/helloworld.git package/custom-package/ssr-plus
-# git clone https://github.com/vernesong/OpenClash.git package/custom-package/OpenClash
-
-# git clone https://github.com/yichya/luci-app-xray.git package/custom-package/luci-app-xray
-# git clone https://github.com/Mitsuhaxy/luci-i18n-xray-zh-cn.git package/custom-package/luci-i18n-xray-zh-cn
-# git clone https://github.com/pymumu/smartdns.git package/custom-package/smartdns
-# git clone https://github.com/sirpdboy/luci-app-netdata.git package/custom-package/luci-app-netdata
 
 # 添加核心温度的显示
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
