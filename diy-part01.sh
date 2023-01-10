37a68be11dddc566541f7cc364ffdb6d924b98da
hululu1068 <xjianglin@gmail.com>
2023-01-10 21:13:45 +0800

    update openwrt-x86_64-Official.yml

--- diy-part01.sh
+++ diy-part01.sh
@@ -17,3 +17,26 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+
+# themes添加（svn co 命令意思：指定版本如https://github）
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/custom-package/luci-theme-argon
+git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon.git package/custom-package/luci-theme-atmaterial-ColorIcon
+git clone https://github.com/thinktip/luci-theme-neobird.git package/custom-package/luci-theme-neobird
+
+# 添加额外软件包
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/custom-package/luci-app-adguardhome
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/custom-package/openwrt-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/custom-package/luci-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/custom-package/luci-passwall2
+git clone --depth=1 https://github.com/fw876/helloworld.git package/custom-package/ssr-plus
+git clone https://github.com/vernesong/OpenClash.git package/custom-package/OpenClash
+
+git clone https://github.com/yichya/luci-app-xray.git package/custom-package/luci-app-xray
+git clone https://github.com/Mitsuhaxy/luci-i18n-xray-zh-cn.git package/custom-package/luci-i18n-xray-zh-cn
+git clone https://github.com/pymumu/smartdns.git package/custom-package/smartdns
+git clone https://github.com/sirpdboy/luci-app-netdata.git package/custom-package/luci-app-netdata
+
+# Add UPX
+cd ~/work/Compiled-OpenWrt-Firmware/Compiled-OpenWrt-Firmware/tools/upx-3.96-amd64_linux
+sudo chmod +x upx
+sudo cp ./upx ~/work/Compiled-OpenWrt-Firmware/Compiled-OpenWrt-Firmware/openwrt/staging_dir/host/bin/
